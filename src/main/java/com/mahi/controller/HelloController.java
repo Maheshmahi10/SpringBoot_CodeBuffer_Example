bbfbe3d41679be32767c013c0ac52a6c7a60191a
Maheshmahi10 <maheshgowda978@gmail.com>
2021-07-15 14:10:40 +0530

    Createdhellocontroller

---
+++ src/main/java/com/mahi/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.mahi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping(value= "/", method=RequestMethod.GET)
+	public String greeting() {
+		return "Welcome Mahesh !!";
+	}
+
+}
